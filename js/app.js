20e86c4082482cedac926828a3906a7c04f6da84
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-05-13 22:56:38 -0700

    solved problemFive tyk

--- js/app.js
+++ js/app.js
@@ -89,18 +89,17 @@ function sumArray(sumArr) { //eslint-disable-line
   let totalSum = 0;
 
   for (let i = 0; i < sumArr.length; i++) {
-    totalSum += sum(totalSum, sumArr[i])[0];
+    totalSum = sum(totalSum, sumArr[i])[0];
   }
 
-  let myStr = `${sumArr} was passed in as an array of numbers, and 9 is their sum.`;
+  let myStr = `${sumArr} was passed in as an array of numbers, and ${totalSum} is their sum.`;
   return [totalSum, myStr];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
-sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -116,11 +115,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let totalProduct = 1;
 
+  for (let i = 0; i < multArr.length; i++) {
+    totalProduct = multiply(totalProduct, multArr[i])[0];
+  }
+
+  let myStr = `The numbers ${multArr} have a product of ${totalProduct}.`;
+  return [totalProduct, myStr];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
+
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
